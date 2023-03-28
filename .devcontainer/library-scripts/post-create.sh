0f8a489e7de8f2a44f5bfe4a60175f4df48d3fc2
avsthiago <avsthiago@gmail.com>
2023-03-28 20:55:31 +0000

    Improve the post-create script

--- .devcontainer/library-scripts/post-create.sh
+++ .devcontainer/library-scripts/post-create.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 
 set -ex 
-k3d cluster list | grep -q k3s-default || (k3d cluster create mycluster > k3d.log 2>&1 && export KUBECONFIG=$(k3d kubeconfig write mycluster))
 
+# Fix the permissions of the project files
+sudo find /workspaces/kopylot -not -user vscode -exec chown vscode:vscode {} \;
+
+# Creates the cluster if it doesn't exist
+k3d cluster list | grep -q k3s-default || k3d cluster create mycluster > k3d.log 2>&1
+
+# Stores the kube configurations in the ~/.kube/config
 mkdir -p ~/.kube/
 k3d kubeconfig get k3s-default > ~/.kube/config
 
+# Installs Poetry
 curl -sSL https://install.python-poetry.org | python3 -
