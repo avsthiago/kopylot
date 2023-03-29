89b0507c6f179f96110b80992fa8e23952445d47
avsthiago <avsthiago@gmail.com>
2023-03-29 19:36:20 +0000

    Adding typings

--- tests/test_ctl.py
+++ tests/test_ctl.py
@@ -1,10 +1,11 @@
+from typing import Any
 from unittest.mock import MagicMock, patch
 
 from kopylot.cli import ctl
 
 
 @patch("kopylot.cli.subprocess.run")
-def test_get_data_valid(mock_run) -> None:
+def test_get_data_valid(mock_run: Any) -> None:
     mock_stdout = MagicMock()
     mock_stdout.configure_mock(stdout="No resources found in default namespace.")
     mock_run.return_value = mock_stdout
