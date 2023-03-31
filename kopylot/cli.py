11004a10fa8b4757793716068bf0ba2b73b3a1e8
avsthiago <avsthiago@gmail.com>
2023-03-31 10:23:41 +0000

    Implementing the diagnose and using rich to make it shine

--- kopylot/cli.py
+++ kopylot/cli.py
@@ -1,7 +1,10 @@
 import subprocess
 from typing import List
 
+import rich
 import typer
+from rich.panel import Panel
+from rich.text import Text
 
 from .diagnose import run_diagnose
 
@@ -24,17 +27,35 @@ def diagnose(
         ..., help="The type of resource to diagnose. Must be one of: pod, deployment, service"
     ),
     resource_name: str = typer.Argument(..., help="The name of the resource to diagnose"),
+    show_describe: bool = typer.Option(False, "--show-describe", help="Show the output of `kubectl describe`"),
+    no_color: bool = typer.Option(False, "--no-color", help="Disable color output and borders"),
 ) -> str:
     """
     Diagnose a pod, deployment, or service using a LLM model.
     """
-    # run kubectl describe for the resource
+    # run `kubectl describe` for the resource
     describe_command = f"kubectl describe {resource_type} {resource_name}"
     describe_result = subprocess.run(describe_command, shell=True, capture_output=True)
-    print(describe_result)
+    describe_decoded_result = describe_result.stdout.decode("utf-8")
+    if show_describe:
+        if no_color:
+            print(describe_decoded_result)
+        else:
+            rich.print(Panel(Text(describe_decoded_result), title=f"{resource_type.title()} description"))
 
-    diagnose_result = run_diagnose(resource_type, describe_result.stdout.decode("utf-8"))
-    print(diagnose_result)
+    diagnose_result: str = run_diagnose(resource_type, describe_result.stdout.decode("utf-8"))
+
+    if no_color:
+        print(f"\nDiagnosis for the {resource_type.title()} {resource_name}:\n{diagnose_result}")
+        return diagnose_result
+
+    rich.print(
+        Panel(
+            Text(diagnose_result),
+            title=f"\U0001f916 Diagnosis for the {resource_type.title()} {resource_name}",
+            style="bold green",
+        )
+    )
     return diagnose_result
 
 
--- kopylot/diagnose.py
+++ kopylot/diagnose.py
@@ -1,11 +1,8 @@
-import os
 import re
 
-import openai
+from kopylot import llm
 
 from . import prompts
-
-openai.api_key = os.getenv("OPENAI_API_KEY", "")
 
 # Regular expression pattern to match common errors in pod descriptions
 error_pattern = re.compile(r"\b(unsupported|invalid|missing|failed|error)\b", re.IGNORECASE)
@@ -13,14 +10,8 @@ error_pattern = re.compile(r"\b(unsupported|invalid|missing|failed|error)\b", re
 
 def run_diagnose(resource_type: str, describe_result: str) -> str:
     prompt = prompts.diagnose_prompt(resource_type, describe_result)
-    response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=prompt,
-        temperature=0.7,
-        max_tokens=1024,
-    )
+    solution = llm.ask_llm(prompt)
 
-    solution: str = response.choices[0].text.strip()
     if error_pattern.search(solution):
         return solution
     else:
