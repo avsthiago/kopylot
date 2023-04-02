f3c44bff4584349fcd0a6f481e25e749f26b3bde
avsthiago <avsthiago@gmail.com>
2023-04-02 13:10:04 +0000

    Remove the output constraints for the diagnose

--- kopylot/diagnose.py
+++ kopylot/diagnose.py
@@ -1,16 +1,6 @@
-import re
-
 from kopylot import llm, prompts
-
-# Regular expression pattern to match common errors in pod descriptions
-error_pattern = re.compile(r"\b(unsupported|invalid|missing|failed|error)\b", re.IGNORECASE)
 
 
 def run_diagnose(resource_type: str, resource_description: str) -> str:
     prompt = prompts.diagnose_prompt(resource_type, resource_description)
-    solution = llm.ask_llm(prompt)
-
-    if error_pattern.search(solution):
-        return solution
-    else:
-        return "No errors detected"
+    return llm.ask_llm(prompt)
