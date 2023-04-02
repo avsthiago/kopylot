25870b56e590044569e341f31a2e17e0f0d4272c
avsthiago <avsthiago@gmail.com>
2023-04-02 13:24:45 +0000

    Increase the mak tokens for the llm

--- kopylot/llm.py
+++ kopylot/llm.py
@@ -5,7 +5,7 @@ import openai
 openai.api_key = os.getenv("OPENAI_API_KEY", "")
 
 
-def ask_llm(prompt: str, temperature: float = 0.7, max_tokens: int = 1024) -> str:
+def ask_llm(prompt: str, temperature: float = 0.7, max_tokens: int = 2048) -> str:
     response = openai.Completion.create(
         engine="text-davinci-003",
         prompt=prompt,
