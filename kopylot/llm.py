d799ebed82ff4080501a89dc0d5c5a5bbd32c0d3
avsthiago <avsthiago@gmail.com>
2023-04-03 18:15:34 +0000

    Changing the API key name

--- kopylot/llm.py
+++ kopylot/llm.py
@@ -2,7 +2,7 @@ import os
 
 import openai
 
-openai.api_key = os.getenv("OPENAI_API_KEY", "")
+openai.api_key = os.getenv("KOPYLOT_AUTH_TOKEN", "")
 
 
 def ask_llm(prompt: str, temperature: float = 0.7, max_tokens: int = 2048) -> str:
